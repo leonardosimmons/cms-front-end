00625840a3be8854f7c52468e7965046be0dacdd
Leonardo Simmons <leonardo.simmons07@gmail.com>
2021-02-04 20:56:10 -0800

    First commit

---
+++ src/store/keys.ts
@@ -0,0 +1,15 @@
+//* Keys
+
+/**
+ * Keys for all of the dispatch 'actions' within the current store
+ */
+export enum ActionKeys {
+
+};
+
+/**
+ * Keys for each element within the app
+ */
+export enum ElementKeys {
+
+};
