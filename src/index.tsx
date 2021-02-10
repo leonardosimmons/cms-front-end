179834d200eb5b1519364692f46d91c73f785b5c
Leonardo Simmons <leonardo.simmons07@gmail.com>
2021-02-10 00:59:44 -0800

    tailwind configs

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import '../src/styles';
-import '../src/styles/scss';
 import App from './containers/app';
 import { store } from './store';
 import { Provider } from 'react-redux';
